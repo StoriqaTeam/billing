ebcbded9d287d6a34cc74929874a9e845a2fdcf3
Semyon Ivanov <semyon95@gmail.com>
2019-01-14 11:19:11 +0300

    refactor event handling

--- src/event_handling/handlers.rs
+++ src/event_handling/handlers.rs
@@ -2,17 +2,28 @@ use diesel::{connection::AnsiTransactionManager, pg::Pg, Connection};
 use futures::future;
 use r2d2::ManageConnection;
 
-use models::invoice_v2::InvoiceId;
+use models::{invoice_v2::InvoiceId, Event, EventPayload};
 use repos::repo_factory::ReposFactory;
 
-use super::{Context, EventHandlerFuture};
+use super::{EventHandler, EventHandlerFuture};
 
-// TODO: handle this event properly
-pub fn handle_invoice_paid<T, M, F>(_ctx: Context<T, M, F>, _invoice_id: InvoiceId) -> EventHandlerFuture<()>
+impl<T, M, F> EventHandler<T, M, F>
 where
     T: Connection<Backend = Pg, TransactionManager = AnsiTransactionManager> + 'static,
     M: ManageConnection<Connection = T>,
     F: ReposFactory<T>,
 {
-    Box::new(future::ok(()))
+    pub fn handle_event(self, event: Event) -> EventHandlerFuture<()> {
+        let Event { id: _, payload } = event;
+
+        match payload {
+            EventPayload::NoOp => Box::new(future::ok(())),
+            EventPayload::InvoicePaid { invoice_id } => self.handle_invoice_paid(invoice_id),
+        }
+    }
+
+    // TODO: handle this event properly
+    pub fn handle_invoice_paid(self, _invoice_id: InvoiceId) -> EventHandlerFuture<()> {
+        Box::new(future::ok(()))
+    }
 }
