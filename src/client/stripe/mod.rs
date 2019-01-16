dde8a8c10475e43db70209745a86a0f6671a63b4
busarovalex <busarovalex@yandex.ru>
2019-01-16 12:08:10 +0300

    Make stripe client methods async

--- src/client/stripe/mod.rs
+++ src/client/stripe/mod.rs
@@ -1,8 +1,11 @@
 mod error;
 mod types;
 
-use futures::{Future, IntoFuture};
-use stripe::{CaptureParams, Charge, ChargeParams, Customer, CustomerParams, PaymentSourceParams};
+use futures::Future;
+use futures_cpupool::CpuPool;
+use stripe::{
+    CaptureParams, Charge, ChargeParams, Customer, CustomerParams, PaymentIntent, PaymentIntentCreateParams, PaymentSourceParams,
+};
 
 use self::types::*;
 use config;
@@ -26,6 +29,8 @@ pub trait StripeClient: Send + Sync + 'static {
     fn refund(&self, input: NewRefund) -> Box<Future<Item = Refund, Error = Error> + Send>;
 
     fn create_payout(&self, input: NewPayOut) -> Box<Future<Item = PayOut, Error = Error> + Send>;
+
+    fn create_payment_intent(&self, input: PaymentIntentCreateParams) -> Box<Future<Item = PaymentIntent, Error = Error> + Send>;
 }
 
 #[derive(Clone)]
@@ -33,63 +38,70 @@ pub struct StripeClientImpl {
     public_key: String,
     secret_key: String,
     client: stripe::Client,
+    cpu_pool: CpuPool,
 }
 
 impl StripeClientImpl {
-    pub fn create_from_config(config: config::Config) -> Self {
+    pub fn create_from_config(config: config::Config, cpu_pool: CpuPool) -> Self {
         let secret_key = config.stripe.secret_key.clone();
         let client = stripe::Client::new(secret_key.clone());
         Self {
             public_key: config.stripe.public_key.clone(),
             secret_key,
             client,
+            cpu_pool,
         }
     }
 }
 
 impl StripeClient for StripeClientImpl {
     fn create_customer(&self, input: NewCustomer) -> Box<Future<Item = Customer, Error = Error> + Send> {
-        Box::new(
-            Customer::create(
-                &self.client,
-                CustomerParams {
-                    email: Some(&input.email),
-                    ..Default::default()
-                },
-            )
-            .map_err(From::from)
-            .into_future(),
-        )
-    }
-    fn create_customer_with_source(&self, input: NewCustomerWithSource) -> Box<Future<Item = Customer, Error = Error> + Send> {
-        Box::new(
-            Customer::create(
-                &self.client,
-                CustomerParams {
-                    email: Some(&input.email),
-                    source: Some(PaymentSourceParams::Token(input.token)),
-                    ..Default::default()
-                },
-            )
-            .map_err(From::from)
-            .into_future(),
-        )
-    }
-    fn get_customer(&self, customer_id: CustomerId) -> Box<Future<Item = Customer, Error = Error> + Send> {
-        Box::new(
-            Customer::retrieve(&self.client, &customer_id.inner())
+        Box::new(self.cpu_pool.spawn_fn({
+            let client = self.client.clone();
+            move || {
+                Customer::create(
+                    &client,
+                    CustomerParams {
+                        email: Some(&input.email),
+                        ..Default::default()
+                    },
+                )
                 .map_err(From::from)
-                .into_future(),
-        )
+            }
+        }))
     }
+
+    fn create_customer_with_source(&self, input: NewCustomerWithSource) -> Box<Future<Item = Customer, Error = Error> + Send> {
+        Box::new(self.cpu_pool.spawn_fn({
+            let client = self.client.clone();
+            move || {
+                Customer::create(
+                    &client,
+                    CustomerParams {
+                        email: Some(&input.email),
+                        source: Some(PaymentSourceParams::Token(input.token)),
+                        ..Default::default()
+                    },
+                )
+                .map_err(From::from)
+            }
+        }))
+    }
+
+    fn get_customer(&self, customer_id: CustomerId) -> Box<Future<Item = Customer, Error = Error> + Send> {
+        Box::new(self.cpu_pool.spawn_fn({
+            let client = self.client.clone();
+            move || Customer::retrieve(&client, &customer_id.inner()).map_err(From::from)
+        }))
+    }
+
     fn create_charge(&self, input: NewCharge) -> Box<Future<Item = Charge, Error = Error> + Send> {
-        Box::new(
-            input
-                .currency
-                .convert()
-                .and_then(|currency| {
+        Box::new(self.cpu_pool.spawn_fn({
+            let client = self.client.clone();
+            move || {
+                input.currency.convert().and_then(|currency| {
                     Charge::create(
-                        &self.client,
+                        &client,
                         ChargeParams {
                             amount: Some(input.amount.inner() as u64),
                             currency: Some(currency),
@@ -99,23 +111,32 @@ impl StripeClient for StripeClientImpl {
                     )
                     .map_err(From::from)
                 })
-                .into_future(),
-        )
+            }
+        }))
     }
     fn get_charge(&self, charge_id: ChargeId) -> Box<Future<Item = Charge, Error = Error> + Send> {
-        Box::new(Charge::retrieve(&self.client, &charge_id.inner()).map_err(From::from).into_future())
+        Box::new(self.cpu_pool.spawn_fn({
+            let client = self.client.clone();
+            move || Charge::retrieve(&client, &charge_id.inner()).map_err(From::from)
+        }))
     }
     fn capture_charge(&self, charge_id: ChargeId) -> Box<Future<Item = Charge, Error = Error> + Send> {
-        Box::new(
-            Charge::capture(&self.client, &charge_id.inner(), CaptureParams { ..Default::default() })
-                .map_err(From::from)
-                .into_future(),
-        )
+        Box::new(self.cpu_pool.spawn_fn({
+            let client = self.client.clone();
+            move || Charge::capture(&client, &charge_id.inner(), CaptureParams { ..Default::default() }).map_err(From::from)
+        }))
     }
     fn refund(&self, _input: NewRefund) -> Box<Future<Item = Refund, Error = Error> + Send> {
         unimplemented!()
     }
     fn create_payout(&self, _input: NewPayOut) -> Box<Future<Item = PayOut, Error = Error> + Send> {
         unimplemented!()
+    }
+
+    fn create_payment_intent(&self, input: PaymentIntentCreateParams) -> Box<Future<Item = PaymentIntent, Error = Error> + Send> {
+        Box::new(self.cpu_pool.spawn_fn({
+            let client = self.client.clone();
+            move || PaymentIntent::create(&client, input).map_err(From::from)
+        }))
     }
 }
