fc4c66efff9eff32a8d09c290d7517661a1dcc92
busarovalex <busarovalex@yandex.ru>
2019-01-30 18:00:20 +0300

    warn error

--- src/services/stripe.rs
+++ src/services/stripe.rs
@@ -83,8 +83,10 @@ impl<
         let fut = spawn_on_pool(db_pool, cpu_pool, move |conn| {
             let event_store_repo = repo_factory.create_event_store_repo_with_sys_acl(&conn);
             conn.transaction(move || {
-                let event = Webhook::construct_event(event_payload, signature_header, secret)
-                    .map_err(ectx!(try ErrorContext::StripeClient, ErrorKind::Internal))?;
+                let event = Webhook::construct_event(event_payload, signature_header, secret).map_err(|e| {
+                    warn!("stripe Webhook::construct_event error: {:?}", e);
+                    ectx!(try err e, ErrorKind::Internal)
+                })?;
                 info!("stripe handle_stripe_event event: {:?}", event);
 
                 match extract_stripe_event(event)? {
