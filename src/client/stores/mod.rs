12fee71461fd8dcd441c36fdeb78065c29fb7a93
Serejka <5125402@mail.ru>
2019-02-02 19:05:21 +0300

    fixed stores default headers

--- src/client/stores/mod.rs
+++ src/client/stores/mod.rs
@@ -8,6 +8,7 @@ use failure::Fail;
 use futures::Future;
 use hyper::{Headers, Method};
 use stq_http::client::HttpClient;
+use stq_http::request_util::{Currency as CurrencyHeader, FiatCurrency as FiatCurrencyHeader};
 
 pub trait StoresClient: Send + Sync + 'static {
     fn get_currency_exchange(&self) -> Box<Future<Item = CurrencyExchangeInfoRequest, Error = Error> + Send>;
@@ -25,13 +26,20 @@ impl<C: HttpClient + Clone + Send> StoresClientImpl<C> {
     }
 }
 
+fn stores_headers() -> Headers {
+    let mut headers = Headers::new();
+    headers.set(CurrencyHeader("STQ".to_string()));
+    headers.set(FiatCurrencyHeader("USD".to_string()));
+    headers
+}
+
 impl<C: HttpClient + Clone> StoresClient for StoresClientImpl<C> {
     fn get_currency_exchange(&self) -> Box<Future<Item = CurrencyExchangeInfoRequest, Error = Error> + Send> {
         let StoresClientImpl { client, url } = self.clone();
         let url = format!("{}/currency_exchange", url);
 
         let fut = client
-            .request_json::<CurrencyExchangeInfoRequest>(Method::Get, url.clone(), None, None)
+            .request_json::<CurrencyExchangeInfoRequest>(Method::Get, url.clone(), None, Some(stores_headers()))
             .map_err(ectx!(ErrorSource::StqHttp, ErrorKind::Internal => Method::Get, url, None as Option<Headers>));
 
         Box::new(fut)
